fdb1885544d2f48ec45c3a9e4169acca5c47d50b
Ogar <kamil.sobczyk@wp.pl>
2019-02-11 07:17:16 +0100

    adding a list element to a store fixed

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,15 +1,13 @@
 const reducer = (state, action) => {
   switch (action.type) {
     case "INFO_DIALOG":
-    console.log(action);
+      console.log(action);
       return { ...state, openInfo: state.openInfo ? false : true };
     case "ADD_DIALOG":
       return { ...state, openAdd: state.openAdd ? false : true };
-    case "ADD_ITEM": 
-    console.log('ADD_ITEM in reducer fired')
-    const newList = state.list;
-    newList.push(action.newItem);
-    return { ...state, list: newList };
+    case "ADD_ITEM":
+      console.log("ADD_ITEM in reducer fired");
+      return { ...state, list: [...state.list, action.newItem ] };
     default:
       return state;
   }
