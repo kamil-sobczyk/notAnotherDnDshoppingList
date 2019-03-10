38bf11a250d70758196752dba6d4414d049f7d76
Ogar <kamil.sobczyk@wp.pl>
2019-03-10 14:42:01 +0100

    path to reducer changed

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 
 import { createStore } from "redux";
 import { Provider } from "react-redux";
-import reducer from "./reducers/reducer";
+import reducer from "./reducers";
 
 import App from "./app";
 
