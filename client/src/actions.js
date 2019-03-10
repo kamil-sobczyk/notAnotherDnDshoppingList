46c3d41d03a88a764754c75c691b4bd45afe5a03
Ogar <kamil.sobczyk@wp.pl>
2019-03-10 15:29:18 +0100

    acion creators prepared

--- client/src/actions.js
+++ client/src/actions.js
@@ -9,3 +9,76 @@ const GET_ITEMS = "GET_ITEMS";
 const GET_SELECTED = "GET_SELECTED";
 const GET_COSTS = "GET_COSTS";
 const ADD_COST = "ADD_COST";
+
+const toggleShowItems = () => {
+  return {
+    type: TOGGLE_SHOW_ITEMS
+  };
+};
+const toggleShowAddDialog = () => {
+  return {
+    type: TOGGLE_SHOW_ADD_DIALOG
+  };
+};
+const toggleShowDeleteDialog = () => {
+  return {
+    type: TOGGLE_SHOW_DELETE_DIALOG
+  };
+};
+const toggleShowEditDialog = () => {
+  return {
+    type: TOGGLE_SHOW_EDIT_DIALOG
+  };
+};
+const addItem = item => {
+  return {
+    type: ADD_ITEM,
+    newItem: item
+  };
+};
+const deleteItem = activeItem => {
+  return {
+    type: DELETE_ITEM,
+    index: activeItem.index
+  };
+};
+const editItem = (newItem, activeItem) => {
+  return {
+    type: EDIT_ITEM,
+    newItem: newItem,
+    index: activeItem.index,
+    list: activeItem.list
+  };
+};
+const getItems = items => {
+  return {
+    type: GET_ITEMS,
+    items
+  };
+};
+const getSelected = selected => {
+  return {
+    type: GET_SELECTED,
+    selected
+  };
+};
+const getCosts = costs => {
+    return {
+        type: GET_COSTS,
+        costs
+    };
+};
+const addCost = costs => {
+    return {
+        type: ADD_COST,
+        costs
+    };
+};
+
+export {
+  toggleShowItems,
+  toggleShowAddDialog,
+  toggleShowDeleteDialog,
+  toggleShowEditDialog,
+  addItem
+};
