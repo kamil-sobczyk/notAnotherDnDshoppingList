0cc2cb2e9f0b697fe31c0d52550b11cf20b93b62
Ogar <kamil.sobczyk@wp.pl>
2019-03-12 21:21:44 +0100

    dialogsTransition renamed to be more generic

--- client/src/components/dialogs/dialogsTransition.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import Slide from "@material-ui/core/Slide";
-
-const Transition = props => {
-    return <Slide direction="up" {...props} />;
-};
-
-export default Transition;
