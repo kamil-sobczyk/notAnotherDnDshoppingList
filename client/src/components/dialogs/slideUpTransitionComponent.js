a13e53244bf79f52ce57e5f9a20b94ca850efd3a
Ogar <kamil.sobczyk@wp.pl>
2019-03-12 21:20:33 +0100

    transition component in separete file used

---
+++ client/src/components/dialogs/slideUpTransitionComponent.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import Slide from "@material-ui/core/Slide";
+
+const Transition = props => {
+    return <Slide direction="up" {...props} />;
+};
+
+export default Transition;
